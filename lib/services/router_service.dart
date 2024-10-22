e6043ce3f9fbd70d9ec68c4d800afe4307eeebe1
Mohamed Ashraf <62084282+m7mda4raf99@users.noreply.github.com>
2024-10-22 16:02:47 +0300

    Created/initiated all screens

--- lib/services/router_service.dart
+++ lib/services/router_service.dart
@@ -1,7 +1,11 @@
 import 'package:social_feed_app/main.dart';
+import 'package:social_feed_app/views/home_screen.dart';
+import 'package:social_feed_app/views/login_screen.dart';
 
 class RouterService {
   static final routes = {
     '/': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
+    '/login': (context) => const LoginScreen(),
+    '/home': (context) => const HomeScreen(),
   };
 }
---
+++ lib/views/marketplace_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MarketPlaceScreen extends StatelessWidget {
+  const MarketPlaceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
